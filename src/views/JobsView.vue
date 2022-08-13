a2e752e7b9179e994c2a73dc9128bc6309bcd145
towsifzilani <towsifzilani@gmail.com>
2022-08-13 17:34:27 +0600

    VUe Router Jobs

---
+++ src/views/JobsView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Job Lists</h1>
+    <div v-for="job in jobs" :key="job.id">
+        <h4>{{ job.title }}</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jobs:[
+                {id: 1, title:"First Job", details: 'lorem 1'},
+                {id: 2, title:"second Job", details: 'lorem 2'},
+                {id: 3, title:"third Job", details: 'lorem 3'}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
