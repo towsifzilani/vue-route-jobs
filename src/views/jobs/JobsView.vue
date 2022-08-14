567be6d6fde5cc973c6e94bcc9ede504e7f10661
towsifzilani <towsifzilani@gmail.com>
2022-08-14 23:26:53 +0600

    VUe Router: dynamic links

--- src/views/jobs/JobsView.vue
+++ src/views/jobs/JobsView.vue
@@ -4,6 +4,7 @@
     <div v-for="job in jobs" :key="job.id" class="job">
         <router-link :to="{ name: 'JobsDetails', params:{ id: job.id } }">
             <h2>{{ job.title }}</h2>
+            <p></p>
         </router-link>
     </div>
   </div>
