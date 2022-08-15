1b7927620333ba4bb527aba98d46e4b9cbfe0688
towsifzilani <towsifzilani@gmail.com>
2022-08-15 23:59:38 +0600

    VUe Router: dynamic links with parameter

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, dignissimos! Alias optio aspernatur vel quae sit voluptas ad quos harum. Totam et quidem exercitationem at nulla, animi error praesentium, necessitatibus maiores, corrupti nihil temporibus in. Possimus accusantium, numquam alias fuga ipsa quia dicta qui obcaecati ullam cum id corrupti eius recusandae laudantium vitae inventore.</p>
   </div>
 </template>
 
